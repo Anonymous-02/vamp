64d0d1d2ecdf686feb6ec72649c4e2a3bb798ca9
remi <62241664+Anonymous-02@users.noreply.github.com>
2021-09-07 18:55:42 +0200

    Add files via upload

---
+++ src/main/java/net/mcreator/vampire/ClientProxyVampire.java
@@ -0,0 +1,26 @@
+package net.mcreator.vampire;
+
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.client.model.obj.OBJLoader;
+
+public class ClientProxyVampire implements IProxyVampire {
+	@Override
+	public void init(FMLInitializationEvent event) {
+	}
+
+	@Override
+	public void preInit(FMLPreInitializationEvent event) {
+		OBJLoader.INSTANCE.addDomain("vampire");
+	}
+
+	@Override
+	public void postInit(FMLPostInitializationEvent event) {
+	}
+
+	@Override
+	public void serverLoad(FMLServerStartingEvent event) {
+	}
+}
